e92c3bdd3b9bf3f6aea7f24e517f6de916fa6074
Lluis Campos <lluis.campos@northern.tech>
2024-05-28 13:32:37 +0200

    wip: Migrate test image to Raspberry Pi OS Bullseye
    Ticket: QA-691
    
    Signed-off-by: Lluis Campos <lluis.campos@northern.tech>

--- docker/docker-files-raspbian/prepare-raspbian-img.sh
+++ docker/docker-files-raspbian/prepare-raspbian-img.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-# Downloads Raspbian SD image and prepares it for testing (enable SSH and add trusted key)
+# Downloads Raspberry Pi OS image and prepares it for testing (create user, enable SSH and add trusted key)
 # Downloads also the dependencies (kernel and dtb file) for QEMU emulation
 
 set -e -x
 
 show_help_and_exit() {
   cat << EOF
-Usage: $0 raspbian-version
+Usage: $0 raspios-version
 
 Arguments:
-    raspbian-version    - Official Raspbian Buster version, for example 2019-07-10
+    raspios-version    - Official Raspberry Pi OS Bullseye version, for example 2023-05-03
 EOF
   exit 1
 }
@@ -47,55 +47,62 @@ if [ -f ${currdir}/${raspbian_mender_filename_img} ]; then
     exit 0
 fi
 
-# Get superuser privilages to be able to mount the SD image
+# Get superuser privileges to be able to mount the SD image
 sudo true
 
 cd ${workdir}
 
-# For some reason, Raspbian version 2019-04-08 is in a folder named 2019-04-09
-if [ ${version} = "2019-04-08" ]; then
-    raspbian_url="https://downloads.raspberrypi.org/raspbian_lite/images/raspbian_lite-2019-04-09/${raspbian_filename_zip}"
-# For some reason, Raspbian version 2019-07-10 is in a folder named 2019-07-12
-elif [ ${version} = "2019-07-10" ]; then
-    raspbian_url="https://downloads.raspberrypi.org/raspbian_lite/images/raspbian_lite-2019-07-12/${raspbian_filename_zip}"
-else
-    raspbian_url="https://downloads.raspberrypi.org/raspbian_lite/images/raspbian_lite-${version}/${raspbian_filename_zip}"
-fi
+raspbian_url="https://downloads.raspberrypi.org/raspios_lite_armhf/images/raspios_lite_armhf-2023-05-03/2023-05-03-raspios-bullseye-armhf-lite.img.xz"
+raspbian_filename_zip="2023-05-03-raspios-bullseye-armhf-lite.img.xz"
+raspbian_filename_img="2023-05-03-raspios-bullseye-armhf-lite.img"
+raspbian_mender_filename_img="2023-05-03-raspios-bullseye-armhf-lite-mender-testing.img"
 
-echo "##### Donwloading and extracting..."
+echo "##### Downloading and extracting..."
 wget -q -nc ${raspbian_url}
-unzip ${raspbian_filename_zip}
-rm ${raspbian_filename_zip}
-wget -q -nc https://raw.githubusercontent.com/dhruvvyas90/qemu-rpi-kernel/master/kernel-qemu-4.19.50-buster
-wget -q -nc https://raw.githubusercontent.com/dhruvvyas90/qemu-rpi-kernel/master/versatile-pb-buster.dtb
+unxz ${raspbian_filename_zip}
+wget -q -nc https://github.com/dhruvvyas90/qemu-rpi-kernel/raw/master/kernel-qemu-5.10.63-bullseye
+wget -q -nc https://github.com/dhruvvyas90/qemu-rpi-kernel/raw/master/versatile-pb-bullseye-5.10.63.dtb
 
 echo "##### Preparing image for tests..."
-boot_start=$(fdisk -l ${raspbian_filename_img} | grep Linux | tr -s ' ' | cut -d ' ' -f2)
 sector_size=$(fdisk -l ${raspbian_filename_img} | grep '^Sector' | cut -d' ' -f4)
-offset=$(expr $boot_start \* $sector_size)
-mkdir -p img-rootfs
-sudo mount -o loop,offset=$offset ${raspbian_filename_img} img-rootfs
+boot_start=$(fdisk -l ${raspbian_filename_img} | grep W95 | tr -s ' ' | cut -d ' ' -f2)
+boot_offset=$(expr $boot_start \* $sector_size)
+rootfs_start=$(fdisk -l ${raspbian_filename_img} | grep Linux | tr -s ' ' | cut -d ' ' -f2)
+rootfs_offset=$(expr $rootfs_start \* $sector_size)
 
+# Tweaks for the boot partition
+mkdir -p img-boot
+sudo mount -o loop,offset=$boot_offset ${raspbian_filename_img} img-boot
+
+sudo touch img-boot/ssh
+sudo tee img-boot/userconf.txt > /dev/null << EOF
+pi:$(openssl passwd "securepassword")
+EOF
+
+sleep 1
+sudo umount img-boot
+rmdir img-boot
+
+# Tweaks for the rootfs
+mkdir -p img-rootfs
+sudo mount -o loop,offset=$rootfs_offset ${raspbian_filename_img} img-rootfs
+
+# Raspberry Pi OS comes with this oneshot service to generate SSH keys that requires /dev/hwrng, which although
+# existing it is unusable in QEMU. See the sources:
+# https://github.com/RPi-Distro/raspberrypi-sys-mods/blob/bullseye/debian/raspberrypi-sys-mods.regenerate_ssh_host_keys.service
+# https://github.com/RPi-Distro/raspberrypi-sys-mods/blob/bullseye/usr/lib/raspberrypi-sys-mods/regenerate_ssh_host_keys
+sudo sed -i 's|dd|true #dd|' img-rootfs/usr/lib/raspberrypi-sys-mods/regenerate_ssh_host_keys
 sudo mkdir img-rootfs/home/pi/.ssh
 cat ${scriptdir}/../ssh-keys/key.pub | sudo tee img-rootfs/home/pi/.ssh/authorized_keys
-sudo ln -s /lib/systemd/system/ssh.service img-rootfs/etc/systemd/system/multi-user.target.wants/ssh.service
 
-# Allow APT Suite change in the release info
-#
-# Upstream repository http://raspbian.raspberrypi.org/raspbian has changed buster
-# release suite from "stable" to "oldstable". The following configuration option
-# allows APT to update without user interaction.
-# This is the default un newer versions of apt, see:
-#   https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=931566
-#
-echo 'Acquire::AllowReleaseInfoChange::Suite "true";' | sudo tee img-rootfs/etc/apt/apt.conf.d/99release-change-suite
-
+sleep 1
 sudo umount img-rootfs
 rmdir img-rootfs
 
+echo "##### Copying modified files..."
 mv ${raspbian_filename_img} ${currdir}/${raspbian_mender_filename_img}
-mv kernel-qemu-4.19.50-buster ${currdir}/
-mv versatile-pb-buster.dtb ${currdir}/
+mv kernel-qemu-5.10.63-bullseye ${currdir}/
+mv versatile-pb-bullseye-5.10.63.dtb ${currdir}/
 
 cd ${currdir}
 rm -rf ${workdir}
